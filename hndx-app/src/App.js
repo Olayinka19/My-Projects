5ec511a8751b43c07e625604518724532828245f
fakanbiomowunmi@gmail.com <fakanbiomowunmi@gmail.com>
2022-06-11 11:59:01 -0400

    Hndx

---
+++ hndx-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import Nav from './Components/Nav';
+import './App.css';
+import { BrowserRouter as  Router , Routes , Route , Link} from "react-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+   
+    </div>
+  );
+}
+
+export default App;
---
+++ hndx-app/src/Components/Nav.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useState } from 'react';
+import {Link} from "react-dom";
+export default function Nav() {
+  return (
+    
+    <div className='Nav'>
+      
+   <p className='home'>Home</p>
+   
+   <p className='about'>About</p>
+   <h1 className='contact'>Contact</h1>
+   <h1 className='catge'>Categories</h1>
+   <h1 className='wallet'>Wallet</h1>
+   <h1 className='shop'>Shop</h1>
+   <h1 className='chk'>CheckOut</h1>
+  
+  
+    </div>
+  
+  )
+}
