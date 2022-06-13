8700013f2c845b278d0773bc17023124bd3194e4
fakanbiomowunmi@gmail.com <fakanbiomowunmi@gmail.com>
2022-06-13 18:06:43 -0400

    css-grid

---
+++ css-grid-display/src/Components/Container.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+export default function Container(props) {
+  return (
+    <div className=' wrapper'>
+     
+      <p className='one'>
+      <img width="100px" src="https://randomwordgenerator.com/img/picture-generator/g5900afbace889449de13b9517d62c4523c263e41fb608327c8cc26f1ad4e5b85f9414b61f23590fe1db589b27a043bf2_640.jpg"></img>
+      </p>
+      <p className='two'>  <img width="100px" src="https://randomwordgenerator.com/img/picture-generator/g5900afbace889449de13b9517d62c4523c263e41fb608327c8cc26f1ad4e5b85f9414b61f23590fe1db589b27a043bf2_640.jpg"></img></p>
+      <p className='three'> <img width="100px" src="https://randomwordgenerator.com/img/picture-generator/g5900afbace889449de13b9517d62c4523c263e41fb608327c8cc26f1ad4e5b85f9414b61f23590fe1db589b27a043bf2_640.jpg"></img></p>
+      <p className='four'> <img width="100px" src="https://randomwordgenerator.com/img/picture-generator/g5900afbace889449de13b9517d62c4523c263e41fb608327c8cc26f1ad4e5b85f9414b61f23590fe1db589b27a043bf2_640.jpg"></img></p>
+      <p className='five'> <img width="100px" src="https://randomwordgenerator.com/img/picture-generator/g5900afbace889449de13b9517d62c4523c263e41fb608327c8cc26f1ad4e5b85f9414b61f23590fe1db589b27a043bf2_640.jpg"></img></p>
+      <p className='six'> <img width="100px" src="https://randomwordgenerator.com/img/picture-generator/g5900afbace889449de13b9517d62c4523c263e41fb608327c8cc26f1ad4e5b85f9414b61f23590fe1db589b27a043bf2_640.jpg"></img></p>
+      <p className='seven'> <img width="100px" src="https://randomwordgenerator.com/img/picture-generator/g5900afbace889449de13b9517d62c4523c263e41fb608327c8cc26f1ad4e5b85f9414b61f23590fe1db589b27a043bf2_640.jpg"></img></p>
+    </div>
+  )
+}
